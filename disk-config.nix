81fc894efea529ab05821985519c644eee14c219
aiteron <38859510+aiteron@users.noreply.github.com>
2025-12-07 03:32:44 +0500

    Luks removed

--- disk-config.nix
+++ disk-config.nix
@@ -17,18 +17,12 @@
                 mountOptions = [ "umask=0077" ];
               };
             };
-            luks = {
+            root = {
               size = "100%";
               content = {
-                type = "luks";
-                name = "crypted";
-                settings.allowDiscards = true;
-                passwordFile = "/tmp/secret.key";
-                content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/";
-                };
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
               };
             };
           };
