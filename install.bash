79f13fc15ffe03a5765784ecb4e7cf4e7a5bd4bd
Aiteron <sashablinov1998@gmail.com>
2025-12-08 11:51:14 +0500

    Added nix configs (draft)

--- install.bash
+++ install.bash
@@ -11,3 +11,17 @@ sed 's/<DISKID>/'"$diskid"'/g' ./disko/disk-config-normal.nix > disk-config.nix
 
 nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disk-config.nix
 
+echo "2. Configuring NixOS"
+
+nixos-generate-config --root /mnt
+
+cp configuration.nix /mnt/etc/nixos/
+cp home.nix /mnt/etc/nixos/
+cp flake.nix /mnt/etc/nixos/
+
+nixos-install --flake /mnt/etc/nixos#nixos-btw
+
+## type your password
+nixos-enter --root /mnt -c 'passwd tony'
+reboot
+
