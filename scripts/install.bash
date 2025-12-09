7c088977fb4044e2f9a20552fef5acebdef08945
Aiteron <sashablinov1998@gmail.com>
2025-12-10 03:05:44 +0500

    Fixed install script

--- scripts/install.bash
+++ scripts/install.bash
@@ -1,39 +1,40 @@
 #!/bin/bash
 
 echo "1. Configuring the disk"
-cp $(dirname "$0")/../nix/installation/disko-config.nix .
+disko_config=$(cat $(dirname "$0")/../nix/installation/disko-config.nix)
 
 echo "Available disks:"
 lsblk | grep disk
 read -p "Choose disk id for install OS: " diskid
 
-sed 's/<DISKID>/'"$diskid"'/g' disko-config.nix > disko-config.nix
+echo "$disko_config" | sed 's/<DISKID>/'"$diskid"'/g' > disko-config.nix
 
-nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disko-config.nix
+nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount disko-config.nix
 
 echo "2. Configuring basic NixOS config"
 nixos-generate-config --root /mnt
-cp $(dirname "$0")/../nix/installation/configuration.nix .
+configuration=$(cat $(dirname "$0")/../nix/installation/configuration.nix)
 
 read -p "Enter the hostname (name of device in network) : " hostname
-sed 's/<TTT>/'"$hostname"'/g' configuration.nix > configuration.nix
+configuration=$(echo "$configuration" | sed 's/<HOSTNAME>/'"$hostname"'/g')
 
 read -p "Enter the username : " username
-sed 's/<USERNAME>/'"$username"'/g' configuration.nix > configuration.nix
+configuration=$(echo "$configuration" | sed 's/<USERNAME>/'"$username"'/g')
 
 read -p "Enter the timezone (leave blank for default ('America/New_York')) : " timezone
-sed 's/<TIMEZONE>/'"$timezone"'/g' configuration.nix > configuration.nix
+if [ -z "$timezone" ]; then
+    timezone="America/New_York"
+fi
+configuration=$(echo "$configuration" | sed 's@<TIMEZONE>@'"$timezone"'@g')
 
-state_version=$(grep "stateVersion" /mnt/etc/nixos/configuration.nix | cut -d'=' -f2 | tr -d ' ";')
-sed 's/<STATEVERSION>/'"$state_version"'/g' configuration.nix > configuration.nix
+state_version=$(grep "system.stateVersion = " /mnt/etc/nixos/configuration.nix | cut -d'=' -f2 | sed 's/#.*$//' | tr -d ' ";')
+echo "$configuration" | sed 's/<STATEVERSION>/'"$state_version"'/g' > configuration.nix
 
 cp configuration.nix /mnt/etc/nixos/
 
-sleep 5
-
 nixos-install
 
-echo "Enter password for "$username
+echo "Enter password for ""$username"
 nixos-enter --root /mnt -c 'passwd '"$username"
 
 read -p "Reboot system? (yes/no) " answer
